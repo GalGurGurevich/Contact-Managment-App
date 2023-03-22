040f9c0286b958fce5e539c4aa60048fad1c1690
candidate2.travelfactory@gmail.com <gal.gurevich@gmail.com>
2023-03-22 04:31:28 +0200

    utils func re-write

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,11 +1,9 @@
 export function validatePhone(num) {
     const regexPattern = /^[\d()+]*$/; // number, (), +
-
-    if(!num) return false;
-    if (regexPattern.test(num)) {
-        return true
-    } else {
-        console.log("The input string is not valid.");
-        return false;
-    }
+    let res = false;
+    
+    if (num && regexPattern.test(num)) {
+        res = true
+    } 
+    return res;
 }
